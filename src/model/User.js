65b9a89d6e353729b7c228003c9c0c494f2588de
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-10-14 01:45:30 +0600

    simple upgradation in user model

--- src/model/User.js
+++ src/model/User.js
@@ -40,4 +40,5 @@ const userSchema = new Schema(
     }
 );
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
